1771a511b015fc3f1c3a8d1e1b5ca21b46108e5d
drewkybrown <146992520+drewkybrown@users.noreply.github.com>
2023-12-19 10:19:35 -0500

    create current and location weather component files

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,6 @@
-import React, { useState } from "react";
-import SearchBar from "./components/SearchBar/SearchBar";
-import WeatherDisplay from "./components/WeatherDisplay/WeatherDisplay"; // Import the WeatherDisplay component
-import axios from "axios";
+import React from "react";
 import "./App.css";
 
-function App() {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState("");
-  const [weatherData, setWeatherData] = useState(null);
-
-  const handleSearchSubmit = async (cityName) => {
-    setLoading(true);
-    setError("");
-    try {
-      const apiKey = import.meta.env.VITE_OPENWEATHER_API_KEY;
-
-      const response = await axios.get(
-        `https://api.openweathermap.org/data/3.0/weather?q=${cityName}&appid=${apiKey}&units=imperial`
-        // Use 'units=imperial' for Fahrenheit
-      );
-
-      if (response.data) {
-        setWeatherData(response.data);
-      } else {
-        throw new Error("Weather data not available");
-      }
-    } catch (error) {
-      setError("Error fetching weather data");
-      console.error(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Welcome to Weather Watcher!
-      </h1>
-      <SearchBar onSearchSubmit={handleSearchSubmit} />
-      {loading && <p>Loading...</p>}
-      {error && <p>Error: {error}</p>}
-      {/* Include the WeatherDisplay component here */}
-      <WeatherDisplay weatherData={weatherData} />
-    </div>
-  );
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
-
-export default App;
