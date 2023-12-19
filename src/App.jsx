8cfa291bede8cb971dbfe9befc71e866b18b082b
drewkybrown <146992520+drewkybrown@users.noreply.github.com>
2023-12-19 11:15:04 -0500

    Add div elements to home page

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,19 @@ import "./App.css";
 function App() {
   return (
     <div className="bg-slate-800 h-screen flex justify-center items-start">
-      <div>{/* Location */}</div>
+      <div className="w-1/5 h-1/3 mt-40 bg-yellow-400">
+        <h1 className="text-slate-800 text-2xl col-span-2 bg-red-400 ">
+          Weather Location
+        </h1>
+        {/* Location */}
+      </div>
 
-      <div className="w-1/5 h-1/3 mt-40">{/* Current */}</div>
+      <div className="w-1/3 h-1/3 mt-40 p-10 grid grid cols-2 gap-6 bg-white">
+        <h1 className="text-slate-800 text-2xl col-span-2 bg-blue-300">
+          Current Weather
+        </h1>
+        {/* Current */}
+      </div>
     </div>
   );
 }
