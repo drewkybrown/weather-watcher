c46a62358c378b64c3adde5943a8039723453114
drewkybrown <146992520+drewkybrown@users.noreply.github.com>
2023-12-19 10:53:12 -0500

    create react boilerplate for components, import and export to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
 import React from "react";
+import Current from "./components/Current";
+import Location from "./components/Location";
+
 import "./App.css";
 
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+function App() {
+  return (
+    <div className="bg-slate-800 h-screen flex justify-center items-start">
+      <div>{/* Location */}</div>
+
+      <div className="w-1/5 h-1/3 mt-40">{/* Current */}</div>
+    </div>
+  );
 }
+
+export default App;
---
+++ src/components/Current.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Current() {
+  return <div>Current</div>;
+}
+
+export default Current;
